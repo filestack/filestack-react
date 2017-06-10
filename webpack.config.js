8992e106dfcc5d6800cad19eea4245b9a4a2bc7a
samuxyz <zaza.samuele@gmail.com>
2017-06-10 14:53:04 +0800

    removed warnings through uglifyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = [{
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false,
+        warnings: true,
       },
     }),
   ],
