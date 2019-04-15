baecadf594e740139a4f0fbedc9c729066c4e563
AndrzejSala <andrzej@filestack.com>
2019-04-15 16:48:30 +0200

    chore(update): Update filestack.js & demo

--- examples/demo/src/App.js
+++ examples/demo/src/App.js
@@ -17,8 +17,60 @@ class App extends Component {
     });
   }
 
+  renderComponentInstances() {
+    return <div className="examples">
+      <div className="example example1">
+        <div className="label">Button - overlay mode</div>
+        <ReactFilestack
+          apikey={this.state.apiKey}
+          buttonText="Pick file"
+          buttonClass="example1Btn"
+          onSuccess={this.yourCallbackFunction}
+          options={{
+            maxSize: 2 * 1024 * 1024
+          }}
+        />
+      </div>
+      <div className="example example2">
+        <div className="label">Custom link - overlay mode</div>
+        <ReactFilestack
+          apikey={this.state.apiKey}
+          buttonText="Click"
+          buttonClass="example2Btn"
+          onSuccess={this.yourCallbackFunction}
+          link
+        />
+      </div>
+      <div className="example example3">
+        <div className="label">Button - dropPane mode</div>
+        <ReactFilestack
+          apikey={this.state.apiKey}
+          buttonText="Pick file"
+          buttonClass="example3Btn"
+          onSuccess={this.yourCallbackFunction}
+          options={{
+            displayMode: 'dropPane',
+            container: 'testContainer'
+          }}
+        />
+      </div>
+      <div className="example example4">
+        <div className="label">Button - inline mode</div>
+        <ReactFilestack
+          apikey={this.state.apiKey}
+          buttonText="Pick file"
+          buttonClass="example4Btn"
+          onSuccess={this.yourCallbackFunction}
+          options={{
+            displayMode: 'inline',
+            container: 'testContainer'
+          }}
+        />
+      </div>
+    </div>
+  };
+
   render() {
-    const display = this.state.apiKey.length ? {display: 'block'} : {display: 'none'};
     return (
       <div className="App">
         <h1>
@@ -26,53 +78,7 @@ class App extends Component {
         </h1>
         <input placeholder="Filestack api key" name="apiKey" value={this.state.apiKey} onChange={evt => this.updateInputValue(evt)}/>
         <div className="main">
-          <div className="examples" style={display}>
-            <div className="example example1">
-              <div className="label">Button - overlay mode</div>
-              <ReactFilestack
-                apikey={this.state.apiKey}
-                buttonText="Pick file"
-                buttonClass="example1Btn"
-                onSuccess={this.yourCallbackFunction}
-              />
-            </div>
-            <div className="example example2">
-              <div className="label">Custom link - overlay mode</div>
-              <ReactFilestack
-                apikey={this.state.apiKey}
-                buttonText="Click"
-                buttonClass="example2Btn"
-                onSuccess={this.yourCallbackFunction}
-                link
-              />
-            </div>
-            <div className="example example3">
-              <div className="label">Button - dropPane mode</div>
-              <ReactFilestack
-                apikey={this.state.apiKey}
-                buttonText="Pick file"
-                buttonClass="example3Btn"
-                onSuccess={this.yourCallbackFunction}
-                options={{
-                  displayMode: 'dropPane',
-                  container: 'testContainer'
-                }}
-              />
-            </div>
-            <div className="example example4">
-              <div className="label">Button - inline mode</div>
-              <ReactFilestack
-                apikey={this.state.apiKey}
-                buttonText="Pick file"
-                buttonClass="example4Btn"
-                onSuccess={this.yourCallbackFunction}
-                options={{
-                  displayMode: 'inline',
-                  container: 'testContainer'
-                }}
-              />
-            </div>
-          </div>
+          {this.state.apiKey.length && this.renderComponentInstances()}
           <div id="testContainer" ref={this.testContainer}></div>
         </div>
       </div>
